c42dc94bd8ada599b517b05a7cc0f75200c5e0bb
alittlecross <alittlecross@live.co.uk>
2020-05-31 17:48:56 +0100

    failing tests created for .get function of Users class

--- test/lib/users.js
+++ test/lib/users.js
@@ -1,21 +1,20 @@
 const expect = require('chai').expect
 const mock = require('fetch-mock')
 
-const Residents = require('../../server/lib/residents')
+const Users = require('../../server/lib/users')
 const { data, schema } = require('../support')
 
-describe('Residents', async () => {
-  describe('.get', () => {
+describe('Users', async () => {
+  describe('get', () => {
     // Arrange
-    const city = 'London'
-    const residents = new Residents()
+    const users = new Users()
 
     it('expects an array of objects to be returned', async () => {
       // Arrange
-      mock.get(`https://bpdts-test-app.herokuapp.com/city/${city}/users`, data)
+      mock.get('https://bpdts-test-app.herokuapp.com/users', data)
 
       // Act
-      const result = await residents.get(city)
+      const result = await users.get()
 
       // Assert
       expect(result.length).is.eql(2)
@@ -27,11 +26,11 @@ describe('Residents', async () => {
 
     it('expects a bad response to be handled', async () => {
       // Arrange
-      mock.get(`https://bpdts-test-app.herokuapp.com/city/${city}/users`, 404)
+      mock.get('https://bpdts-test-app.herokuapp.com/users', 404)
 
       try {
         // Act
-        await residents.get(city)
+        await users.get()
       } catch (error) {
         // Assert
         expect(error.message).to.equal('Bad response from server')
@@ -42,7 +41,7 @@ describe('Residents', async () => {
 
     it('expects an array of objects with the correct keys to be returned (genuine API call)', async () => {
       // Act
-      const result = await residents.get(city)
+      const result = await users.get()
 
       // Assert
       if (result.length) {
